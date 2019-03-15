8d45f304a1253dba75861fad18903d3f73d3b607
aryzae <a.aryzae@gmail.com>
2019-03-15 17:48:13 +0900

    Modify Chapter5 Section3

--- MyBingo/app/src/main/java/jp/aryzae/mybingo/MainActivity.java
+++ MyBingo/app/src/main/java/jp/aryzae/mybingo/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // 最大値
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText maxNumberEditText;
     // 最大値の設定ボタン
     private Button registerMaxNumberButton;
+    // 次の数字を出すボタン
+    private Button nextNumberButton;
+    // 現在の数字を表示するTextView
+    private TextView currentNumberTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         // ビューの変数を初期化
         maxNumberEditText = findViewById(R.id.max_number);
         registerMaxNumberButton = findViewById(R.id.register_max_number);
+        nextNumberButton = findViewById(R.id.next_number);
+        currentNumberTextView = findViewById(R.id.current_number);
 
         // 最大値の初期値をEditTextにセットする
         maxNumberEditText.setText("" + maxNumber);
@@ -40,5 +47,28 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MainActivity", "maxNumber: " + maxNumber);
             }
         });
+
+        // 表示中の数字を更新する
+        nextNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickNextNumber();
+            }
+        });
+    }
+
+    // nextNumberButtonがタップされたときの処理
+    private void onClickNextNumber() {
+        Log.d("MainActivity", "onClickNextNumber");
+
+        // 0.0〜74.0(最大値が初期値の場合)の数値を生成する
+        double randomNumber = Math.random() * (maxNumber - 1);
+        // 1〜75(最大値が初期値の場合)の整数値を生成する
+        int nextNumber = (int)randomNumber + 1;
+
+        // nextNumberを画面に表示する
+        currentNumberTextView.setText("" + nextNumber);
+
+        Log.d("MainActivity", "nextNumber: " + nextNumber);
     }
 }
